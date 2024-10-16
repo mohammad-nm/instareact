03d391f49617ca85f66fa1a7183dde271574feb9
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-10-16 20:53:20 +0330

    FIX instaLogin

--- src/pages/api/instagram/handleLogin.ts
+++ src/pages/api/instagram/handleLogin.ts
@@ -2,22 +2,33 @@ import { getLLToken } from "@/services/instagram/getLLToken";
 import { getSLToken } from "@/services/instagram/getSLToken";
 import { NextApiRequest, NextApiResponse } from "next";
 import { sendLLToken } from "@/services/instagram/sendLLToken";
+import { error } from "console";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.code) {
-    const code = req.query.code;
-    const SLToken = await getSLToken(code as string);
-    if (SLToken.data) {
-      const LLToken = await getLLToken(SLToken);
-      if (LLToken.access_token) {
-        const sendToken = await sendLLToken(req.body.state, LLToken);
-        console.log(sendToken);
-      } else if (!LLToken.access_token) {
+  const code = req.query.code;
+  try {
+    if (req.query.code) {
+      const SLToken = await getSLToken(code as string);
+      if (SLToken.data) {
+        const LLToken = await getLLToken(SLToken);
+        if (LLToken.access_token) {
+          const sendToken = await sendLLToken(
+            req.query.state as string,
+            LLToken
+          );
+          console.log(sendToken);
+        } else if (!LLToken.access_token) {
+          return res.status(901).json({ messsage: "didnt get the LLToken!" });
+        }
+      } else if (!SLToken.data) {
+        return res.status(901).json({ messsage: "didnt get the SLToken!" });
       }
-    } else if (!SLToken.data) {
+    } else if (!req.query.code) {
+      return res.status(900).json({ message: "no code" });
     }
-  } else if (req.query.error) {
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error });
   }
 }
