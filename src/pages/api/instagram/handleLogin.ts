5e02dd9a028e4cfca6d09e90e3f2359138daf824
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-10-16 21:34:59 +0330

    FIX instaLogin

--- src/pages/api/instagram/handleLogin.ts
+++ src/pages/api/instagram/handleLogin.ts
@@ -8,30 +8,35 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const code = req.query.code;
-  const state = req.query.state as string;
-  const id = state.split("#_")[0];
+  const state = (req.query.state as string).replace(/#_$/, "");
+  const id = state;
   try {
+    if (!code) {
+      return res.status(400).json({ message: "no code" });
+    }
     if (code) {
       const SLToken = await getSLToken(code as string);
+      if (!SLToken.data) {
+        return res.status(400).json({ messsage: "didnt get the SLToken!" });
+      }
       if (SLToken.data) {
         const LLToken = await getLLToken(SLToken);
+        if (!LLToken.access_token) {
+          return res.status(400).json({ messsage: "didnt get the LLToken!" });
+        }
         if (LLToken.access_token) {
           const sendToken = await sendLLToken(id, LLToken);
-          return res.status(200).json({
-            message: "token has been sent!",
-            LLToken,
-            sendToken,
-          });
-        } else if (!LLToken.access_token) {
-          return res.status(901).json({ messsage: "didnt get the LLToken!" });
+          if (sendToken) {
+            return res.status(200).json({
+              message: "token has been sent!",
+              LLToken,
+              sendToken,
+            });
+          }
         }
-      } else if (!SLToken.data) {
-        return res.status(901).json({ messsage: "didnt get the SLToken!" });
       }
-    } else if (!code) {
-      return res.status(900).json({ message: "no code" });
     }
   } catch (error) {
-    return res.status(200).json({ message: error });
+    return res.status(400).json({ message: error });
   }
 }
