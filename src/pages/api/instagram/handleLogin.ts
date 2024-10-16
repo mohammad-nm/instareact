f8e8861b3e58b122b973f6db177e91fcfe0daff2
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-10-16 12:17:57 +0330

    UPDATE instaLogin

--- src/pages/api/instagram/handleLogin.ts
+++ src/pages/api/instagram/handleLogin.ts
@@ -1,25 +1,8 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import router from "next/router";
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const { code } = req.query;
-//   if (!code) {
-//     return res.status(400).json({ error: "Authorization code not provided" });
-//   }
-//   if (code) {
-//     console.log(code);
-//     router.push("/main");
-//   }
-// }
-
 import { NextApiRequest, NextApiResponse } from "next";
 import {
   getShortLivedToken,
   getLongLivedToken,
-} from "../../../services/instagram/handleLogin";
+} from "@/services/instagram/handleLogin";
 
 import { sendToken } from "@/services/instagram/sendToken";
 
@@ -27,9 +10,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log("API handler called");
   const { code, state } = req.query;
 
-  console.log(code, state);
   if (!code) {
     return res.status(400).json({ error: "Authorization code not provided" });
   }
@@ -38,20 +21,15 @@ export default async function handler(
   }
 
   try {
-    // Step 1: Get the short-lived access token
-    console.log(code);
     const shortLivedTokenData = await getShortLivedToken(code as string);
-    console.log(shortLivedTokenData);
-    // Step 2: Exchange for the long-lived token
     const longLivedTokenData = await getLongLivedToken(
       shortLivedTokenData.access_token
     );
-    console.log(longLivedTokenData);
-
     const saveToken: any = await sendToken(state as string, longLivedTokenData);
-    // Step 3: Return the long-lived access token data
+    console.log(longLivedTokenData);
     return res.status(200).json({ longLivedTokenData, saveToken });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ error: "Failed to retrieve access token" });
   }
 }
