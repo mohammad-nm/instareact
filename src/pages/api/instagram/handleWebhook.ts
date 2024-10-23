e83a89719aab95bff88a0745589fe9b495ce62b8
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-10-23 15:27:13 +0330

    FIX

--- src/pages/api/instagram/handleWebhook.ts
+++ src/pages/api/instagram/handleWebhook.ts
@@ -4,7 +4,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    //handling verification
+    //handling webhook handler verification
     const {
       "hub.mode": mode,
       "hub.challenge": challenge,
@@ -19,7 +19,7 @@ export default async function handler(
     }
   } else if (req.method === "POST") {
     //handling notifs
-    res.status(200);
     console.log(JSON.stringify(req.body.entry[0].messaging));
+    return res.status(200).json({ message: "Recived!" });
   }
 }
