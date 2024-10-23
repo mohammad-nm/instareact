5fead41db8e1aff74ddb44773d621f3ccc0e7caf
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-10-24 01:12:24 +0330

    FIX

--- src/app/main/components/Sidebar.tsx
+++ src/app/main/components/Sidebar.tsx
@@ -1,11 +1,11 @@
 export default function Sidebar({ handleLogOut }: any) {
   return (
     <div className="h-full w-[150px] fixed right-1 top-1 min-[550px]:w-[270px]">
-      <div className="w-full h-auto rounded-md  p-4  justify-center text-[#ededed] bg-[#0A0A0A] [box-shadow:#666666_0px_0px_0px_1px] ">
-        <div className="text-xl ml-2 font-semibold min-[550px]:text-2xl">
+      <div className="w-full h-auto rounded-md justify-center text-[#ededed] bg-[#0A0A0A] [box-shadow:#666666_0px_0px_0px_1px] ">
+        <div className="text-xl ml-3 font-semibold min-[550px]:text-2xl pt-2">
           InstaReact
         </div>
-        <div className="w-[90%] mt-5 grid gap-y-3 ml-8">
+        <div className="w-[90%] mt-5 grid gap-y-3 ml-5 pb-4">
           <div className="flex">
             <div className="w-fit  rounded-lg text-white text-xs min-[550px]:text-base">
               Dark Mode
