5d0df7dc6f83424633d970b0873b84d8f7daa0e5
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-11-02 12:37:16 +0330

    FIX handlewebhook

---
+++ src/pages/api/instagram/getUserInfo.ts
@@ -0,0 +1,37 @@
+import { supabase } from "@/utils/supabaseClient";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(400).json({ message: "Methode not allowed!" });
+  }
+  if (!req.body.id) {
+    return res
+      .status(400)
+      .json({ message: "id not provided", id: req.body.id });
+  }
+  try {
+    const id = req.body.id.toString();
+    console.log(id);
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("instagram, reacts")
+      .eq("instagram->instagram->>user_id::text", id);
+    if (error) {
+      console.error("Supabase query error:", error);
+      return res.status(500).json({ message: "Failed to fetch data", error });
+    }
+    if (data.length === 0) {
+      return res.status(404).json({ message: "No matching data found" });
+    }
+    if (data.length > 1) {
+      return;
+    }
+    return res.status(200).json({ data: data });
+  } catch (error) {
+    return res.status(400).json({ error: error });
+  }
+}
---
+++ src/pages/api/webhook/handleWebhook.ts
@@ -0,0 +1,107 @@
+import { redis } from "@/utils/redisClient";
+import axios, { AxiosResponse } from "axios";
+import { error } from "console";
+import { NextApiRequest, NextApiResponse } from "next";
+
+// Add this interface at the top of the file
+interface React {
+  lookFor: string[];
+  active: boolean;
+  reactTo: string[];
+  message: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    //handling webhook handler verification
+    const {
+      "hub.mode": mode,
+      "hub.challenge": challenge,
+      "hub.verify_token": verifyToken,
+    } = req.query || {};
+    if (mode && process.env.WEBHOOK_VERIFICATION_TOKEN === verifyToken) {
+      res.status(200).send(challenge);
+    } else {
+      res
+        .status(403)
+        .json({ error: "verify token invalid!:", verify_token: verifyToken });
+    }
+  } else if (req.method === "POST") {
+    try {
+      if (!req.body.entry[0].messaging && !req.body.entry[0].changes) {
+        return res
+          .status(400)
+          .json({ message: "No message or comment found!" });
+      }
+      // Check if message exists
+      if (req.body.entry[0].messaging) {
+        const recipentID: string = await req.body.entry[0].id;
+        const senderID: string = req.body.entry[0].messaging[0].sender.id;
+        const messageText: string = req.body.entry[0].messaging[0].message.text;
+        const userInfo: AxiosResponse = await axios.post(
+          "https://instareact-beta.vercel.app/api/getUserInfo",
+          { id: recipentID }
+        );
+        const data: {
+          instagram: {
+            instagram: { access_token: string };
+          };
+          reacts: React[];
+        } = userInfo.data.data[0];
+
+        console.log("data:", data);
+        const access_token: string = data.instagram.instagram.access_token;
+        const reacts: React[] = data.reacts;
+        const foundReact: React | undefined = reacts.find((item: React) => {
+          return (
+            item.lookFor.includes(messageText) &&
+            item.active &&
+            item.reactTo.includes("DM")
+          );
+        });
+
+        if (!foundReact) {
+          return res.status(400).json({ message: "No React found!" });
+        }
+        console.log("founded reactTTTTTTTTTTTTTTTTTTTTTT:", foundReact);
+
+        const message: string = foundReact.message;
+        try {
+          const response: AxiosResponse<any, any> = await axios.post(
+            `https://graph.instagram.com/v21.0/${recipentID}/messages`,
+            {
+              recipient: { id: senderID },
+              message: { text: message },
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${access_token}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          console.log("Message sent:", response.data);
+          res;
+        } catch (error) {
+          return res.status(400).json({ message: "error sending message!" });
+        }
+      }
+
+      // Check if changes exist
+      if (req.body.entry[0].changes) {
+        const recipentID = req.body.entry[0].id;
+        const senderID = req.body.entry[0].changes[0].value.from.id;
+        const messageText = req.body.entry[0].changes[0].value.text;
+        // Handle comments logic here
+      }
+
+      return res.status(200).json({ message: "Received!" });
+    } catch (error) {
+      console.error("Error handling POST request:", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
