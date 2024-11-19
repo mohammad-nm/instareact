bc3bad805a6c77efcd5f88429af99089d1ca11a2
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-11-19 08:50:22 +0330

    revert handleLogin and change in handleLogin to handle errors

--- src/pages/api/instagram/handleLogin.ts
+++ src/pages/api/instagram/handleLogin.ts
@@ -3,92 +3,97 @@ import { getSLToken } from "../../../services/instagram/getSLToken";
 import { getLLToken } from "../../../services/instagram/getLLToken";
 import { sendInstaInfo } from "../../../services/instagram/sendInstaInfo";
 import getProfile from "../../../services/instagram/getProfile";
+import { redirect } from "next/navigation";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const getSend = () => {};
   const code = req.query.code;
   const state = (req.query.state as string).replace(/#_$/, "");
   const id = state;
-  if (!code) {
-    return res.status(400).json({ message: "no code" });
-  }
-
-  const SLToken = await getSLToken(code as string);
-  if (!SLToken.access_token) {
-    return res.status(400).json({
-      messsage: "didnt get the SLToken!",
-      SLToken: SLToken,
-    });
-  }
-
-  const LLToken = await getLLToken(SLToken.access_token as string);
-  if (!LLToken.access_token) {
-    const instaInfo = await getProfile(SLToken.access_token);
-    if (!instaInfo.user_id) {
-      return res.status(400).json({
-        message: "didnt get the instaInfo",
-        instaInfo,
-        SLToken,
-        LLToken,
-      });
-    }
-
-    const instagram = {
-      access_token: SLToken.access_token,
-      expires_in: SLToken.expires_in,
-      token_type: SLToken.token_type,
-      account_type: instaInfo.account_type,
-      profile_picture_url: instaInfo.profile_picture_url,
-      user_id: instaInfo.user_id,
-      username: instaInfo.username,
-    };
-    const sendToSupa = await sendInstaInfo(id, instagram);
-    if (sendToSupa) {
-      res.writeHead(302, {
-        Location: "https://instareact-beta.vercel.app/main",
-      });
-      return res.end();
-    }
-    if (!sendToSupa) {
-      return res.status(400).json({
-        message: "error in sendtoken",
-      });
-    }
-  }
-
-  const instaInfo = await getProfile(LLToken.access_token);
-  if (!instaInfo.user_id) {
-    return res.status(400).json({
-      message: "didnt get the instaInfo",
-      instaInfo,
-      SLToken,
-      LLToken,
-    });
-  }
   try {
-    const instagram = {
-      access_token: LLToken.access_token,
-      expires_in: LLToken.expires_in,
-      token_type: LLToken.token_type,
-      account_type: instaInfo.account_type,
-      profile_picture_url: instaInfo.profile_picture_url,
-      user_id: instaInfo.user_id,
-      username: instaInfo.username,
-    };
-    const sendToSupa = await sendInstaInfo(id, instagram);
-    if (sendToSupa) {
-      res.writeHead(302, {
-        Location: "https://instareact-beta.vercel.app/main",
-      });
-      res.end();
+    if (!code) {
+      return res.status(400).json({ message: "no code" });
+    }
+    if (code) {
+      try {
+        const SLToken = await getSLToken(code as string);
+        console.log(SLToken);
+        if (!SLToken.access_token) {
+          return res.status(400).json({
+            // messsage: "didnt get the SLToken!",
+            message: "some error occured! try again!",
+            // SLToken: SLToken.access_token,
+          });
+        }
+        if (SLToken.access_token) {
+          try {
+            const LLToken = await getLLToken(SLToken.access_token as string);
+            console.log(LLToken);
+            if (!LLToken.access_token) {
+              return res.status(400).json({
+                // messsage: "didnt get the LLToken!", LLToken
+                message: "some error occured! try again!",
+              });
+            }
+            if (LLToken.access_token) {
+              try {
+                const instaInfo = await getProfile(LLToken.access_token);
+                if (instaInfo.user_id) {
+                  try {
+                    const instagram = {
+                      access_token: LLToken.access_token,
+                      expires_in: LLToken.expires_in,
+                      token_type: LLToken.token_type,
+                      account_type: instaInfo.account_type,
+                      profile_picture_url: instaInfo.profile_picture_url,
+                      user_id: instaInfo.user_id,
+                      username: instaInfo.username,
+                    };
+                    const sendToSupa = await sendInstaInfo(id, instagram);
+                    if (sendToSupa) {
+                      res.writeHead(302, {
+                        Location: "https://instareact-beta.vercel.app/main",
+                      });
+                      res.end();
+                    }
+                  } catch (error) {
+                    return res.status(400).json({
+                      // message: "error in sendtoken",
+                      // error: error,
+                      message: "some error occured! try again!",
+                    });
+                  }
+                }
+              } catch (error) {
+                return res.status(400).json({
+                  // message: "error in instaInfo",
+                  // error: error,
+                  message: "some error occured! try again!",
+                });
+              }
+            }
+          } catch (error) {
+            return res.status(400).json({
+              // message: "error in LLtoken",
+              // error: error,
+              // SLToken: SLToken,
+              message: "some error occured! try again!",
+            });
+          }
+        }
+      } catch (error) {
+        return res.status(400).json({
+          // message: "error in sltoken", error: error
+          message: "some error occured! try again!",
+        });
+      }
     }
   } catch (error) {
     return res.status(400).json({
-      message: "error in sendtoken",
-      error: error,
+      // message: "did nothing", error: error
+      message: "some error occured! try again!",
     });
   }
 }
